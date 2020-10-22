044007bc5678e303a067dd84466afbe28cd8862f
Kevin Chen <kevinrchen0@gmail.com>
2020-10-22 12:49:35 -0400

    Modified plots.ipynb

--- nsc/functions.py
+++ nsc/functions.py
@@ -2,9 +2,10 @@
 import numpy as np
 import pandas as pd
 import math
+from typing import Any  # for NDArray types
 
 
-def CoupledLogarithm(x: float, kappa: float = 0.0, dim: int = 1) -> float:
+def CoupledLogarithm(x: [float, Any], kappa: float = 0.0, dim: int = 1) -> float:
     """
     Generalization of the logarithm function, which defines smooth
     transition to power functions.
@@ -16,16 +17,16 @@ def CoupledLogarithm(x: float, kappa: float = 0.0, dim: int = 1) -> float:
     dim : The dimension of x, or rank if x is a tensor. Not needed?
     """
     assert dim > 0, "dim must be greater than 0."
-    assert x >= 0, "x must be greater or equal to 0."  # Greater than 0?????
-    # dim = np.linalg.matrix_rank(x)
-    # risk_bias = -kappa / (1 + dim*kappa)
-    risk_bias = kappa / (1 + dim*kappa)  # risk bias ratio
-    # risk_bias = (-2*k) / (1 + k)  # Negative sign take inverse of func
-    # coupled_log_value = 0
+    if isinstance(x, float):    
+        assert x >= 0, "x must be greater or equal to 0."  # Greater than 0?????
+    else:
+        assert isinstance(x, np.ndarray), "x must be a np.ndarray type if a sequence, or a float if a scalar."
+        # assert np.all(x), "all values in x must be "
+
     if kappa == 0:
         coupled_log_value = np.log(x)  # divide by 0 if x == 0
     else:
-        # coupled_log_value = (1/-kappa) * (x**risk_bias - 1)
+        risk_bias = kappa / (1 + dim*kappa)  # risk bias ratio
         coupled_log_value = (1 / kappa) * (x**risk_bias - 1)
     return coupled_log_value
 
@@ -42,21 +43,21 @@ def CoupledExponential(x: float, kappa: float = 0.0, dim: int = 1) -> float:
         # may also want to test that dim is an integer
 
         # removed the requirement on kappa; although not common kappa can be less than -1/dim
-    risk_bias = kappa / (1 + dim*kappa)  # risk bias ratio
-    # coupled_exp_value = 0
     if kappa == 0:
         coupled_exp_value = math.exp(x)
-    elif kappa > 0:
-    	coupled_exp_value = (1 + kappa*x)**(1/risk_bias) # removed negative sign and added reciprocal
-    # now given that kappa < 0
-    elif (1 + kappa*x) >= 0:
-   		coupled_exp_value = (1 + kappa*x)**(1/risk_bias) # removed negative sign and added reciprocal
-    elif (risk_bias) > 0: # removed negative sign
-   		coupled_exp_value = 0
     else:
-   		coupled_exp_value = float('inf')
-    # else:
-    # 	print("Error: kappa = 1/d is not greater than -1.")
+        risk_bias = kappa / (1 + dim*kappa)  # risk bias ratio    
+        if kappa > 0:
+        	coupled_exp_value = (1 + kappa*x)**(1/risk_bias) # removed negative sign and added reciprocal
+        # now given that kappa < 0
+        elif (1 + kappa*x) >= 0:
+       		coupled_exp_value = (1 + kappa*x)**(1/risk_bias) # removed negative sign and added reciprocal
+        elif (risk_bias) > 0: # removed negative sign
+       		coupled_exp_value = 0
+        else:
+       		coupled_exp_value = float('inf')
+        # else:
+        # 	print("Error: kappa = 1/d is not greater than -1.")
     return coupled_exp_value
 
 
@@ -68,7 +69,7 @@ def MultivariateCoupledExpotentialDistribution(x, k, mu, sigma):
     pass
 
 
-def CoupledNormalDistribution(x, k, mu, sigma):
+def CoupledNormalDistribution(x, mean, std, kappa, alpha):
     """
     Short description
     ----------
@@ -76,7 +77,8 @@ def CoupledNormalDistribution(x, k, mu, sigma):
     k :
     d :
     """
-    pass
+    assert std >= 0, "std must be greater than or equal to 0."
+    assert alpha in [1, 2], "alpha must be set to either 1 or 2."
 
 
 def MultivariateCoupledDistribution(x, k, mu, sigma):
