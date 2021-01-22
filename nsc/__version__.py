9dc930b7e4e5267256a84dd7fac3f39ee728b872
Kevin Chen <kevinrchen0@gmail.com>
2021-01-21 20:10:53 -0500

    Fixed the CoupledNormal vs. Student-t tests for examples/guide_v0.ipynb.

--- nsc/__version__.py
+++ nsc/__version__.py
@@ -2,9 +2,9 @@
 """Specifies the version of the Nonlinear-Statistical-Coupling package."""
 
 __title__ = 'nsc'
-__author__ = 'Kevin R. Chen, Kenric P. Nelson, Daniel Svoboda, John Clements, and William Thistleton'
+__author__ = 'Kevin R. Chen, Kenric P. Nelson, John Clements, Daniel Svoboda, and William Thistleton'
 __description__ = 'Library of core functions for the statistics of nonlinear systems.'
 __url__ = 'https://github.com/kenricnelson/Nonlinear-Statistical-Coupling.git'
 __version__ = '0.0.2'
 __license__ = 'Apache License 2.0'
-__copyright__ = '2020 Kevin R. Chen, Kenric P. Nelson, Daniel Svoboda, John Clements, and William Thistleton'
+__copyright__ = '2020 Kevin R. Chen, Kenric P. Nelson, John Clements, Daniel Svoboda, and William Thistleton'
