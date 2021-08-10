3b63e99709c5a3df41e6482ad04a926aa4af648e
Hong Xiang <hxyue1@gmail.com>
2021-08-10 09:46:51 -0700

    tidying up files

--- nsc_tf/distributions/coupled_normal_testing1.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-sys.path.insert(0, '/home/hongxiang/Documents/repos/Nonlinear-Statistical-Coupling/')
-
-from nsc.distributions.coupled_normal_tf import CoupledNormal as CoupledNormal_tf
