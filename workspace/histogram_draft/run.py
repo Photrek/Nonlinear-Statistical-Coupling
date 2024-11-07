4772c941ee0a8d305e1e50ffb51f352ea5947271
amenahma <91977258+amenahma@users.noreply.github.com>
2024-11-07 15:38:53 +0300

    Update run.py

--- workspace/histogram_draft/run.py
+++ workspace/histogram_draft/run.py
@@ -95,7 +95,7 @@ def main():
 
     Path(f"{tb_logger.log_dir}/Samples").mkdir(exist_ok=True, parents=True)
     Path(f"{tb_logger.log_dir}/Reconstructions").mkdir(exist_ok=True, parents=True)
-    Path(f"{tb_logger.log_dir}/z").mkdir(exist_ok=True, parents=True)  # إنشاء مجلد لـ z_samples
+    Path(f"{tb_logger.log_dir}/z").mkdir(exist_ok=True, parents=True)  
 
 
     print(f"======= Training {config['model_params']['name']} =======")
